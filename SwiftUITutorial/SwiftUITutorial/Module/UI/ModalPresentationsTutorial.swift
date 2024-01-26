c7fb60d3f6ae0beac8e72e06dd09564d99c7bef1
Luke <luke@wangxutech.com>
2024-01-26 22:23:47 +0800

    feat: modal

---
+++ SwiftUITutorial/SwiftUITutorial/Module/UI/ModalPresentationsTutorial.swift
@@ -0,0 +1,18 @@
+//
+//  ModalPresentationsTutoral.swift
+//  SwiftUITutorial
+//
+//  Created by zcj on 2024/1/26.
+//
+
+import SwiftUI
+
+struct ModalPresentationsTutorial: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModalPresentationsTutorial()
+}
